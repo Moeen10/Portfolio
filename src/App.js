7e397f5018ff31665020b3239291559ec224868c
Moeen10 <umoeen3@gmail.com>
2021-09-26 22:23:31 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Shop from './components/Shop/Shop';
+
+import Header from './Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header />
+      <Shop/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <div className="head">
+                <h1>Baby Shop</h1>
+                <p>
+                    We give the original products,and we provide good Service         
+               </p>
+               <h1>Your spend total : <span>500 Doller</span> </h1>
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
