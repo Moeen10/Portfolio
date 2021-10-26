a17c13ffd6fc2df5246ace859cd831d26b212f9e
Moeen10 <umoeen3@gmail.com>
2021-10-26 13:00:11 +0600

    Header & route Set

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Shop from './components/Shop/Shop';
+import Buses from './components/Buses/Buses';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Profile from './components/Profile/Profile';
+// import Shop from './components/Shop/Shop';
 
 import Header from './Header/Header';
 
 function App() {
   return (
     <div className="App">
-
-      <Header />
-      <Shop/>
+<BrowserRouter>
+<Header/>
+<Switch>
+  <Route path="/login">
+    <Login/>
+  </Route>
+  <Route path="/home">
+  <Home/>
+  </Route>
+  <Route path="/bus">
+  <Buses/>
+  </Route>
+  <Route path="/profile">
+  <Profile/>
+  </Route>
+</Switch>
+</BrowserRouter>
+    
+    
     </div>
   );
 }
---
+++ src/components/Buses/Buses.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Buses = () => {
+    return (
+        <div>
+            <h1>Its Bus component</h1>
+        </div>
+    );
+};
+
+export default Buses;
